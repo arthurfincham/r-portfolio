18bccc067108cc1207d3b0a0ab44058ee0b432aa
Arthur Fincham <arthurfincham@me.com>
2021-11-29 22:26:57 +0000

    further speed optimisation

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module RPortfolio
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.middleware.insert_after ActionDispatch::Static, Rack::Deflater
     # Configuration for the application, engines, and railties goes here.
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # These settings can be overridden in specific environments using the files
